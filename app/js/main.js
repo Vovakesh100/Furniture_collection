9bf1dab53ceba9547b077e56941609ecb308af5e
vova <vovakesh100@gmail.com>
2023-09-25 11:31:21 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+	$('.header__btn').on('click', function () {
+		$('.rightside-menu').removeClass('rightside-menu--close');
+	});
+	$('.rightside-menu__close').on('click', function () {
+		$('.rightside-menu').addClass('rightside-menu--close');
+	});
+	$('.top__slider').slick({
+		dots: true,
+		arrows: false,
+		fade: true,
+		autoplay: true,
+	});
+	$('.contact-slider').slick({
+		slidesToShow: 10,
+		slidesToScroll: 10,
+		dots: true,
+		arrows: false,
+	});
+	$('.article-slider__box').slick({
+		prevArrow:
+			'<button class="article-slider__arrow article-slider__arrowleft" type="button"><img src="images/arrow-slide-left.svg" alt="arrow left"></button>',
+		nextArrow:
+			'<button class="article-slider__arrow article-slider__arrowright" type="button"><img src="images/arrow-slide-right.svg" alt="arrow right"></button>',
+	});
+
+	const mixer = mixitup('.gallery__inner', {
+		load: {
+			filter: '.living',
+		},
+	});
+});
